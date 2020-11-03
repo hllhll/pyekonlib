e5ae08f7e356114b53fbe34f11975a0c54028b00
hll <hll>
2020-11-03 11:22:28 +0200

    Maybe this would make migration work?

--- pyekonlib/Migration.py
+++ pyekonlib/Migration.py
@@ -6,6 +6,7 @@ def sendCommand(deviceAddr, command, postData):
     url = "http://%s/config?command=%s" % (deviceAddr, command)
     headers = {'User-Agent': 'LuaSocket 2.0.2', 'Content-Type': 'application/json; charset=utf-8', 'Host': "1.1.1.1"}
     r = requests.post(url, headers=headers, data=json.dumps(postData, separators=(',', ':')))
+    r.close()
     return r.status_code == requests.codes.ok
 
 
