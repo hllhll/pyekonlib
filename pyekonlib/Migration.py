5628f407e190ce357bc95d78e42959a2d29f7002
hll <hll>
2020-11-10 23:32:41 +0200

    Maybe now migration works this way?

--- pyekonlib/Migration.py
+++ pyekonlib/Migration.py
@@ -1,13 +1,35 @@
 import requests
 import json
-
+import socket
 
 def sendCommand(deviceAddr, command, postData):
-    url = "http://%s/config?command=%s" % (deviceAddr, command)
+    path = "/config?command=%s" % (command)
     headers = {'User-Agent': 'LuaSocket 2.0.2', 'Content-Type': 'application/json; charset=utf-8', 'Host': "1.1.1.1"}
-    r = requests.post(url, headers=headers, data=json.dumps(postData, separators=(',', ':')))
-    r.close()
-    return r.status_code == requests.codes.ok
+    req_pqyload = json.dumps(postData, separators=(',', ':'))
+
+    headers["Content-Length"] = len(req_pqyload)
+
+    header_rows = (": ".join((k, str(headers[k]))) for k in headers.keys())
+    headers = "\r\n".join(header_rows)
+
+    request_str = "POST " + path + " HTTP/1.1\r\n" + \
+        headers + "\r\n" + \
+        "\r\n" + \
+        req_pqyload
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((deviceAddr, 80))
+    client.send(request_str.encode())
+
+    # deviceAddr
+    response = client.recv(4096)
+    str_resp = str(response)
+
+    # TODO, The device has uniqe fingerpring whereas it does not have space between the HTTP headers and data, like
+    """HTTP/1.1 200 OK
+    Content-Type:text/html
+    Content-Length:0"""
+    return str_resp.find("200 OK") != -1
 
 
 def SetDeviceUDPServer(deviceAddr, serverAddr, serverPort):
